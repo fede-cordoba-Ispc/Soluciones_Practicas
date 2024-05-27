4a5ba352154742a1f54e64dac21e4fc50295d029
fede-cordoba-ispc <federico.cordoba.ispc@gmail.com>
2024-05-26 22:51:21 -0300

    prueba

---
+++ prueba.py
@@ -0,0 +1,13 @@
+
+# input y print
+Nombre = input("Ingrese su nombre:")
+print(Nombre)
+
+#valores constantes
+PI = 3.14159
+
+#calculo del Area
+radio = 5
+Area_circunferencia = PI * radio**2
+
+print(Area_circunferencia)
